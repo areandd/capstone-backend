00c987ff2d7033b998c4e934ec194c413cb32810
koonzinc <williamgeorgekoonz@gmail.com>
2022-10-27 21:46:59 +0000

    update routes

--- src/main.py
+++ src/main.py
@@ -8,11 +8,19 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Posts
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 import hashlib
 #from models import Person
 
 app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+jwt = JWTManager(app)
+
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -55,6 +63,48 @@ def handle_signup():
         db.session.add(user)
         db.session.commit()
         return jsonify('Success'), 200
+
+@app.route('/login', methods=['POST'])
+def handle_login():
+
+    requestBody = request.get_json(force=True)
+    email = requestBody['email']
+    hash_password = hashlib.sha224(requestBody['password'].encode("UTF-8")).hexdigest()
+    checkUser = User.query.filter_by(email = email, password = hash_password).first()
+    if checkUser:
+        user_token = create_access_token(identity = checkUser.id)
+        return jsonify(user_token), 200
+    else:
+        return jsonify('User does not exist'), 401
+
+@app.route('/validate-token', methods=['GET'])
+
+@jwt_required()
+
+def handle_token():
+    token = get_jwt_identity()
+    return jsonify(loggedInAs = token), 200
+
+@app.route('/posts', methods=['GET'])
+
+@jwt_required()
+
+def handle_getPosts():
+    posts = Posts.query.all()
+    allPosts = list(map(lambda post: post.serialize(), posts))
+    return jsonify(allPosts)
+
+
+
+
+@app.route('/posts', methods=['POST'])
+
+
+
+
+
+
+
 
 
 
